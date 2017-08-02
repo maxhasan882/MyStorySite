cb74598dae7f2245f22e3d5ef39ddab0ae16b34b
Jayed Al Hasan <jayed.al.hasan@gmail.com>
2017-08-02 16:11:48 +0600

    Created new MVC and Test project

---
+++ MyStorySite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyStorySite.Startup))]
+namespace MyStorySite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
